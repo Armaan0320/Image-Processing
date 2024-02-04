dc018c98ac98e146177d8ef3e726eafc8a0f986a
Armaan <armaanjan1689@gmail.com>
2024-02-04 21:44:12 +0530

    converted coloured image to grayscale

--- file1.py
+++ file1.py
@@ -3,6 +3,7 @@ import numpy
  
 #reading an image
 img = cv2.imread("img/fruits.png")
+'''
 #to show the type of image - that is - numpy array
 print(type(img))
 
@@ -11,5 +12,10 @@ print(img.shape)
 
 #to know the size fo the image
 print(img.size)
-cv2.imshow("window", img)
+'''
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+print(img_gray.shape)
+
+#cv2.imshow("window", img)
+cv2.imshow("window", img_gray)
 cv2.waitKey(0)
