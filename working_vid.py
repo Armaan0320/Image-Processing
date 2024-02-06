5007f2537d93447bb89dab8ac0edc5421960dacd
Armaan <armaanjan1689@gmail.com>
2024-02-06 22:26:02 +0530

    Saving video from webcam

--- working_vid.py
+++ working_vid.py
@@ -2,12 +2,21 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter("output.avi", fourcc, 20.0, (1280,720))
 
 while True:
     ret, frame = cap.read() #return will tell wheather the video is stopped or running.
+    out.write(frame)
     # print(ret)
+
+    #To convert image to grayscale in webcam
+    # img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY   )
+    
+    # cv2.imshow("webcam", img_gray)
     cv2.imshow("webcam", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('x'):
         break
+out.release()
 cv2.destroyAllWindows()
