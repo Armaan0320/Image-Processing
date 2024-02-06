ef4a47102551a2a405892ec5105c22823cc8199f
Armaan <armaanjan1689@gmail.com>
2024-02-06 19:37:41 +0530

    circle drawn with left click

--- live_direct_drawing.py
+++ live_direct_drawing.py
@@ -8,12 +8,17 @@ with a single or double click of your mouse or any keyboard key.'''
 def draw(event, x, y, flags, params):
     # print("Event triggered")
     # print(event)
-    if event == 0:
-        print("Mouse moved")
-    elif event == 1:
-        print("Mouse down clicked")
-    elif event == 4:
-        print("Mouse key released.")
+    # if event == 0:
+    #     print("Mouse moved")
+    # elif event == 1:
+    #     print("Mouse down clicked")
+    # elif event == 4:
+    #     print("Mouse key released.")
+
+    if event ==1:
+        cv2.circle(img, center=(x,y), radius = 50, color =(0,0,255),thickness =-1)
+
+
 
 cv2.namedWindow(winname="window")
 cv2.setMouseCallback("window", draw)
