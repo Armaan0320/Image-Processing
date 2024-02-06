d79d9360898bb7c4be97cf1380e24bcb3af17ae4
Armaan <armaanjan1689@gmail.com>
2024-02-06 19:35:54 +0530

    using events and call back and palying with it.

--- live_direct_drawing.py
+++ live_direct_drawing.py
@@ -1,10 +1,24 @@
 import cv2
 import numpy as np
 
-img = np.zeros(((512,512,3)))
 
 '''live direct drawing, for example drawing any shape
 with a single or double click of your mouse or any keyboard key.'''
+
+def draw(event, x, y, flags, params):
+    # print("Event triggered")
+    # print(event)
+    if event == 0:
+        print("Mouse moved")
+    elif event == 1:
+        print("Mouse down clicked")
+    elif event == 4:
+        print("Mouse key released.")
+
+cv2.namedWindow(winname="window")
+cv2.setMouseCallback("window", draw)
+
+img = np.zeros((512,512,3))
 
 while True: #creates an infinite loop
     cv2.imshow("window", img)
