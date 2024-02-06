71d1280c02ff9970cb97f736a055b784fee5f09c
Armaan <armaanjan1689@gmail.com>
2024-02-06 14:53:21 +0530

    flipping done, for 0,1,-1 codes. Understood its importance in data augmentation in ML.

--- file1.py
+++ file1.py
@@ -30,8 +30,23 @@ print(img.size)
 
 # cv2.imshow("window", new_img)
 
-img_resize = cv2.resize(img, (800,800))
-img_resize = cv2.resize(img, (img.shape[1]//2, img.shape[0]//2))
+# img_resize = cv2.resize(img, (800,800))
+# img_resize = cv2.resize(img, (img.shape[1]//2, img.shape[0]//2))
 
-cv2.imshow("window", img_resize)
+# cv2.imshow("window", img_resize)
+
+#flipping an image
+#flipping an image on x axis
+# img_flip = cv2.flip(img, 0)
+
+#flipping an image on y axis  
+# img_flip = cv2.flip(img, 1)
+
+
+# flipping an image vertically and horizontally
+img_flip = cv2.flip(img, -1)
+
+cv2.imshow("window", img_flip)
+
+
 cv2.waitKey(0)
